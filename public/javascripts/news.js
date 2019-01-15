fc26e9da581aa1ea65e964da296e8b01b3ca0e57
yezunfa <619489025@qq.com>
2019-01-16 00:12:50 +0800

    refresh and search in news

---
+++ public/javascripts/news.js
@@ -0,0 +1,32 @@
+let newsVM = new Vue({
+    el:"#showVue",
+    data:{
+        arr:[],
+        page:0,
+        line:2,
+        keyWord:""
+ },
+    methods:{
+        refresh:function(){
+            this.$http.post('/news',{page:this.page,line:this.line,keyWord:this.keyWord}).then(function(data){
+                console.log(data);
+                this.page+=this.line;
+                this.arr.push(...data.body);
+            })
+        },
+        search:function() {
+            this.page = 0;
+            this.arr = [];
+            this.$http.post('/news', {page: this.page, line: this.line}).then(function (data) {
+                console.log(data);
+                this.page += this.line;
+                this.arr.push(...data.body);
+            })
+        }
+    },
+    //生命周期
+        created:function(){
+         this.refresh();
+
+    }
+});
--- routes/news.js
+++ routes/news.js
@@ -1,16 +1,23 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../models/db');
+var news_sql = "SELECT * FROM news where title like ? limit ? , ?";
 /* GET home page. */
 
 router.get('/', function(req, res) {
-
-    let news_sql = "SELECT * FROM news where name like ?";
-     console.log(news_sql);
-     db.query(news_sql,["%"],function (rows)
-     {
-         res.render('news',{news:rows});
-     });
+     // console.log(news_sql);
+     // db.query(news_sql,["%"],function (rows)
+     // {
+         res.render('news');
+     // });
 });
+router.post('/',function(req,resp){
+    let page=req.body.page;
+    let line=req.body.line;
+    let keyWord=req.body.keyWord;
 
+    db.query(news_sql,["%"+keyWord+"%",page,line],function(rows){
+        resp.json(rows);
+    })
+});
 module.exports = router;
